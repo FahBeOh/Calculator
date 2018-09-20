694ac11c09769804a8f7dc29f3d87ec0f9b1a06e
FahBeOh <fabio.restrepo88@gmail.com>
2018-09-20 16:12:31 -0700

    Front end first draft

---
+++ app.js
@@ -0,0 +1,10 @@
+(() => {
+    const calculator = {
+        cacheDOM: function () {
+            const button = document.getElementsByClassName("btn");
+            const display = document.getElementsByTagName("input");
+        }
+    }
+
+    calculator.cacheDOM();
+})();
