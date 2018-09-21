07d7b5931945b3d532e809d4daebb4837bf72835
FahBeOh <fabio.restrepo88@gmail.com>
2018-09-20 22:01:40 -0700

    Button clicks render to calculator display

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
 (() => {
     const calculator = {
+        string: "",
+        input: [],
+        operators: ["+", "-", "*", "/", "="],
         cacheDOM: function () {
-            const button = document.getElementsByClassName("btn");
-            const display = document.getElementsByTagName("input");
+            this.button = document.getElementsByClassName("btn");
+            this.display = document.getElementById("dis");
+        },
+        bindEvents: function () {
+            for (i = 0; i < this.button.length; i++) {
+                this.button[i].addEventListener("click", this.update);
+            };
+        },
+        initiate: function () {
+            this.cacheDOM();
+            this.bindEvents();
+        },
+        update: function () {
+            calculator.input.push(this.value);
+            console.log(calculator.input)
+            calculator.render()
+        },
+        render: function () {
+            this.string = this.input.join("");
+            this.display.value = "";
+            this.display.value+=this.string;
+            console.log(this.string)
         }
-    }
-
-    calculator.cacheDOM();
+    };
+    calculator.initiate();
 })();
