0577f71e09bd2bfd7d3c3e4a89c301f408c5a1f6
FahBeOh <fabio.restrepo88@gmail.com>
2018-09-22 15:47:10 -0700

    Added comments

--- app.js
+++ app.js
@@ -1,26 +1,32 @@
+// IIFE to initiate object
 (() => {
     const calc = {
+        // "Local Variables"
         num1: "",
         num2: "",
         operator: "",
         result: "",
         operatorIsChosen: false,
         isCalculated: false,
+        // Convience variables
         cacheDOM: function () {
             this.button = document.getElementsByClassName("btn");
             this.opButton = document.getElementsByClassName("btn-danger");
             this.display = document.getElementById("dis");
         },
+        // Binds Events
         bindEvents: function () {
             for (i = 0; i < this.button.length; i++) {
                 this.button[i].addEventListener("click", this.render);
             };
         },
+        // Initiates Calculator
         initiate: function () {
             this.cacheDOM();
             this.bindEvents();
             this.clear()
         },
+        // Log/filter for rendering of input to calculator display
         render: function () {
             if (this.value === "C") {
                 calc.clear();
@@ -43,6 +49,7 @@
             else if (this.value === "=" && calc.num1 !== "" && calc.num2 !== "" && calc.operator !== "")
                 calc.evaluate();
         },
+        // Clears calculator/sets local variable to empty
         clear: function () {
             calc.num1 = "";
             calc.num2 = "";
@@ -51,6 +58,7 @@
             calc.isCalculated = false
             calc.display.value = "";
         },
+        // Empties local variables except display
         clearValues: function () {
             calc.num1 = "";
             calc.num2 = "";
@@ -58,6 +66,7 @@
             calc.operatorIsChosen = false;
             calc.isCalculated = false
         },
+        // Math logic depending on operator
         evaluate: function () {
             let firstNum = parseInt(this.num1);
             let secondNum = parseInt(this.num2);
@@ -77,7 +86,7 @@
             calc.display.value = calc.result;
             calc.clearValues();
         }
-    }
+    };
     calc.initiate();
 })();
 
